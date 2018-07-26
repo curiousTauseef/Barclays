07fd79a49b6b49a5ebe4fed97fe01d0e7aaa72e0
esenav <evaldas.senavaitis@gmail.com>
2018-07-26 17:39:09 +0100

    Updated solution to check if cards are in the list

--- Solution2.java
+++ Solution2.java
@@ -1,15 +1,21 @@
+import java.util.Arrays;
+
 public class Solution2 {
+    private static String[] cards = {"2","3","4","5", "6", "7", "8","J", "Q", "K", "A", "T"};
 
     public static void main(String[] args) {
+
         String A = "A586QK";
         String B = "JJ653K";
-        solution(A, B);
+        if(stringContainsCorrectValues(A, cards)&&stringContainsCorrectValues(B,cards)){
+            solution(A, B);
+        }else System.out.println("Player cards are not correct");
     }
 
     private static int  solution(String A, String B) {
 
         int winnings = 0;
-        String[] cards = {"2","3","4","5", "6", "7", "8", "T", "J", "Q", "K", "A"};
+
         String[] player1 = A.split("");
         String[] player2 = B.split("");
         int index =0 , index2 = 0;
@@ -31,5 +37,21 @@ public class Solution2 {
         }
         System.out.println(winnings);
         return winnings;
+    }
+    private static boolean stringContainsCorrectValues(String playerCards, String[] cards){
+        boolean pass = false;
+        String[] players = playerCards.split("");
+        int count=0;
+        for (String a : cards) {
+            for (String b : players) {
+                if (b.equals(a)) {
+                    count++;
+                }
+            }
+        }
+        if(count == playerCards.length()){
+            pass=true;
+        }
+        return pass;
     }
 }
